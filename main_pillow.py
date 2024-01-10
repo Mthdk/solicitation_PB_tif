3e5d7ac8e5e1fb9aa0d1df67c2960fb4a9d9fa52
Mthdk <matheus.karo@aluno.faculdadeimpacta.com.br>
2024-01-10 10:16:33 -0300

    Update main_pillow.py

--- main_pillow.py
+++ main_pillow.py
@@ -1,5 +1,7 @@
 import os
-from PIL import Image, TiffImagePlugin
+import numpy as np
+from PIL import Image
+import tifffile as tf
 
 def converter_bw_concatenar(input_path, output_path):
     # Verifica se o diretório de saída existe, se não, cria
@@ -9,7 +11,7 @@ def converter_bw_concatenar(input_path, output_path):
     # Lista todos os arquivos .tif ou .tiff no diretório de entrada
     imagens = [file for file in os.listdir(input_path) if file.lower().endswith(('.tif', '.tiff'))]
 
-    imagens_concatenadas = []  # Lista para armazenar caminhos das imagens de cada página
+    imagens_concatenadas = []  # Lista para armazenar imagens de cada página
 
     for imagem_nome in imagens:
         # Caminho completo para a imagem
@@ -25,25 +27,16 @@ def converter_bw_concatenar(input_path, output_path):
             # Converte a página para escala de cinza
             pagina_bw = imagem_tiff.convert("L")
 
-            # Salva a página como um arquivo temporário
-            caminho_pagina_temporario = os.path.join(output_path, f"temp_pagina_{len(imagens_concatenadas)}.tif")
-            pagina_bw.save(caminho_pagina_temporario, format='TIFF')
+            # Adiciona a página à lista
+            imagens_concatenadas.append(np.array(pagina_bw))
 
-            # Adiciona o caminho da página à lista
-            imagens_concatenadas.append(caminho_pagina_temporario)
+    # Concatena as imagens verticalmente
+    imagem_concatenada = np.concatenate(imagens_concatenadas, axis=0)
 
-    # Cria um único arquivo TIFF combinando todas as páginas
+    # Salva a imagem concatenada no diretório de saída em formato TIFF
     nome_arquivo_concatenado = "concatenado.tif"
     caminho_saida_concatenado = os.path.join(output_path, nome_arquivo_concatenado)
-
-    with TiffImagePlugin.AppendingTiffWriter(caminho_saida_concatenado, True) as tf:
-        for caminho_pagina in imagens_concatenadas:
-            # Lê o arquivo temporário e adiciona como um quadro ao arquivo TIFF
-            with open(caminho_pagina, 'rb') as pagina_temporario:
-                tf.write(pagina_temporario.read())
-
-            # Remove o arquivo temporário após adicionar ao TIFF
-            os.remove(caminho_pagina)
+    tf.imwrite(caminho_saida_concatenado, imagem_concatenada, compress=6)  # Você pode ajustar o nível de compressão conforme necessário
 
 if __name__ == "__main__":
     # Diretório de entrada e saída
